4e0ce13085084a246c349064a6d877d66d8e5d8f
tolgaerbas1 <whocanfly01@gmail.com>
2020-04-21 19:54:29 +0300

    q1 finished

--- deneme.asm
+++ deneme.asm
@@ -17,7 +17,8 @@
 	q2E3 : .asciiz "\nEnter the first dimension of first matrix : "
 	q2E4 : .asciiz "\nEnter the second dimension of first matrix : " 
 	q3E1 : .asciiz "\nEnter an input string : "
-	
+	a: .asciiz "a"
+	b: .asciiz "b"
 .text
 	li $s4, 1  #Choices
 	li $s5, 2
@@ -58,35 +59,62 @@ question1:
 	syscall
 	li $v0, 5	        #changing the mode to read integer
 	syscall
-	move $t5, $v0
+	move $t6, $v0
 	li $s0, 1 # a = 1
 	li $s1, 1 # b = 1
+	
+	li $v0, 4	#printing a
+	la $a0, a
+	syscall
+	la $a0, space
+	li $v0, 4
+	syscall
+	li $v0, 4	#printing b
+	la $a0, b
+	syscall
+	la $a0, skip
+	li $v0, 4
+	syscall
 	
 	li $v0, 1	#printing a
 	la $a0, ($s0)
 	syscall
+	la $a0, space
+	li $v0, 4
+	syscall
 	li $v0, 1	#printing b
 	la $a0, ($s1)
 	syscall
-	
-	move $t0, $s0
-	move $t1, $s1
-loop: 	
-	beq $zero, $t5, mainMenu
-	
-	add $t2, $t1,$t1  # 2 * b
-	add $t3, $t0, $t2 # a2 = a + 2*b   # t3 = new A
-	add $t4, $t0, $t1 # b2 = a + b
-	li $v0, 1	#printing a
-	la $a0, ($t3)
+	la $a0, skip
+	li $v0, 4
 	syscall
-	li $v0, 1	#printing b
+	
+	addi $t6, $t6, -1
+loop: 	
+	beq $t6,$zero, mainMenu
+	move $t1, $s0
+	move $t2, $s1
+	
+	add $t3, $t2,$t2  # 2 * b
+	add $t4, $t1, $t3 # a2 = a + 2*b   # t3 = new A
+	add $t5, $t1, $t2 # b2 = a + b
+	li $v0, 1	#printing a
 	la $a0, ($t4)
 	syscall
-	move $s0, $t3
-	move $s1, $t4
-	sub $t5, $t5, -1
+	la $a0, space
+	li $v0, 4
+	syscall
+	li $v0, 1	#printing b
+	la $a0, ($t5)
+	syscall
+	la $a0, skip
+	li $v0, 4
+	syscall
+	move $s0, $t4
+	move $s1, $t5
+	addi $t6, $t6, -1
 	j loop
+	
 question1Print:
 		
 question2:
